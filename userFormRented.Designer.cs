ba8db58109288b9de1a423318db21cc0abbbd3a7
MonteroSportstanacct <rizangelczarveran@gmail.com>
2024-03-13 23:23:37 +0800

    Added Search Function. [ISSUES] not updating [future] admin page

--- userFormRented.Designer.cs
+++ userFormRented.Designer.cs
@@ -28,13 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.rentedMovies = new System.Windows.Forms.DataGridView();
             this.help = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.searchButton = new System.Windows.Forms.Button();
             this.refreshBut = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.rentedMovies)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,14 +47,14 @@
             this.rentedMovies.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
             this.rentedMovies.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.rentedMovies.Cursor = System.Windows.Forms.Cursors.Default;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.rentedMovies.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.rentedMovies.DefaultCellStyle = dataGridViewCellStyle4;
             this.rentedMovies.Location = new System.Drawing.Point(12, 62);
             this.rentedMovies.MultiSelect = false;
             this.rentedMovies.Name = "rentedMovies";
@@ -79,20 +79,11 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 14);
+            this.textBox1.Location = new System.Drawing.Point(177, 14);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(370, 32);
             this.textBox1.TabIndex = 2;
-            // 
-            // searchButton
-            // 
-            this.searchButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.searchButton.Location = new System.Drawing.Point(388, 12);
-            this.searchButton.Name = "searchButton";
-            this.searchButton.Size = new System.Drawing.Size(94, 35);
-            this.searchButton.TabIndex = 3;
-            this.searchButton.Text = "Search";
-            this.searchButton.UseVisualStyleBackColor = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // refreshBut
             // 
@@ -115,14 +106,23 @@
             this.label1.Text = "Not Seeing Your Movie?";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Search By Name: ";
+            // 
             // userFormRented
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1001, 535);
+            this.ClientSize = new System.Drawing.Size(1001, 532);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.refreshBut);
-            this.Controls.Add(this.searchButton);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.help);
             this.Controls.Add(this.rentedMovies);
@@ -135,6 +135,7 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "userFormRented";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.userFormRented_FormClosing);
             this.Load += new System.EventHandler(this.userFormRented_Load);
             ((System.ComponentModel.ISupportInitialize)(this.rentedMovies)).EndInit();
             this.ResumeLayout(false);
@@ -147,8 +148,8 @@
         private System.Windows.Forms.DataGridView rentedMovies;
         private System.Windows.Forms.Button help;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button searchButton;
         private System.Windows.Forms.Button refreshBut;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
